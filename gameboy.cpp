272425cc066026b90a44b87413b91e5a338fb9a4
Jc Tecklenburg <jt9816a@student.american.edu>
2014-12-04 15:50:06 -0500

    dumb error, testrom.gb not in compiled .app

--- gameboy.cpp
+++ gameboy.cpp
@@ -1,6 +1,7 @@
 #include "screen.h"
 #include "Z80.h"
 #include <iostream>
+#include <fstream>
 
 #include <QtGui>
 
@@ -9,7 +10,7 @@ using namespace std;
 //from class INCORRECT
 unsigned char memory[]={0x06,17,0xe,0x16,0x1,0x78,0xb9,0xca,0x15,0x00,0xda,0x15,0x00,0x14,0x78,0x91,0x47,0xc3,0x06,0x00,0x76};
 
-char rom[]={0x06,0x06,0x3e,0x00,0x80,0x05,0xc2,0x04,0x00,0x76};
+char* rom; //[]={0x06,0x06,0x3e,0x00,0x80,0x05,0xc2,0x04,0x00,0x76};
 
 unsigned char memoryread(int address)
 {
@@ -22,15 +23,26 @@ void memorywrite(int address, unsigned char value)
 
 int main()
 {
+	
+
+	
+	ifstream romfile("testrom.gb", ios::in|ios::binary|ios::ate);
+    streampos size = romfile.tellg();
+    rom = new char[size]; //ERROR
+    int romSize=size;
+    romfile.seekg(0,ios::beg);
+    romfile.read(rom,size);
+    romfile.close();
+
 	Z80* z80 = new Z80(memoryread,memorywrite);
 	z80->reset();
-	z80->PC=0;
+	//  z80->PC=0;
 	cout<< "A: "<< z80->A<< " B: "<< z80->B<< " C: "<<z80->C << " D: "<<z80->D <<endl;
 
 	while(!z80->halted){
 		z80->doInstruction();
 		cout<<"A: "<< z80->A<< " B: "<< z80->B<< " C: "<<z80->C << " D: "<<z80->D <<endl;
 	}
-	
+
 	return 0;
 }
